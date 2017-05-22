b1bae671a7002865eba78e3cb4cd8be5f5d3d807
Vlad <vladichnaprimer@gmail.com>
2017-05-22 22:12:46 +0300

    created contact form views

---
+++ app/models/contact.rb
@@ -0,0 +1,14 @@
+class Contact < MailForm::Base
+  attribute :name,      :validate => true
+  attribute :surname,      :validate => true
+  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :message
+
+  def headers
+    {
+      :subject => "My Contact Form",
+      :to => "vladichnaprimer@gmail.com",
+      :from => %("#{name}" <#{email}>)
+    }
+  end
+end
