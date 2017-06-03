5a8288848e5b7ee8091adecd841a3c63e6ccc241
Vlad <vladichnaprimer@gmail.com>
2017-06-03 23:36:36 +0300

    added fake recomender engine

--- app/controllers/faculties_controller.rb
+++ app/controllers/faculties_controller.rb
@@ -1,6 +1,6 @@
 class FacultiesController < ApplicationController
 
     def index
-        @faculties = Faculty.all.order(score: :desc)
+        @faculties = Faculty.recomend_faculty(current_user.score).order(score: :desc).limit(4)
     end
 end
--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -1,3 +1,16 @@
 class Faculty < ActiveRecord::Base
-    
+
+
+    def self.recomend_faculty(user_score)
+        self.all.where('score <= ?', user_score)
+    end
+
+    def euclidean_distance(vector1, vector2)
+      sum = 0
+      vector1.zip(vector2).each do |v1, v2|
+        component = (v1 - v2)**2
+        sum += component
+      end
+      Math.sqrt(sum)
+    end
 end
